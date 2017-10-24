5c01a7bc0601f436bb6077582e33ba2111cf12d8
Steven Smith <stevensmith5@nhs.net>
2017-10-24 16:54:52 +0100

    Able to book from Session Attendances page

--- TDB_SAAS/Controllers/AttendancesController.cs
+++ TDB_SAAS/Controllers/AttendancesController.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using TDB_SAAS.DTOs;
 using TDB_SAAS.Models;
 
 namespace TDB_SAAS.Controllers
@@ -73,17 +74,74 @@ namespace TDB_SAAS.Controllers
 
         // POST: api/Attendances
         [ResponseType(typeof(Attendance))]
-        public async Task<IHttpActionResult> PostAttendance(Attendance attendance)
+        public async Task<IHttpActionResult> PostAttendance(BookingDTO booking)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
+            var sess = await db.Sessions.SingleOrDefaultAsync(s => s.ID == booking.SessID);
+
+            // Session not found
+            if (sess == null)
+            {
+                return BadRequest();
+            }
+
+            // Session Full
+            if (sess.AvailablePlaces < 1)
+            {
+                return BadRequest();
+            }
+
+            // Already Booked
+            if (booking.OutcomeID == 0)
+            {
+                var attnd = await db.Attendances.Where(a => a.OutcomeID == 0 && a.StaffID == booking.StaffID && a.SessID == booking.SessID).ToListAsync();
+                            if (attnd.Count() != 0)
+                            {
+                                return BadRequest();
+                            }
+            }
+
+            Person userperson = db.People.SingleOrDefault(p => p.ID == 0);
+
+            Attendance attendance = new Attendance();
+            attendance.SessID = booking.SessID;
+            attendance.StaffID = booking.StaffID;
+            attendance.OutcomeID = booking.OutcomeID;
+            attendance.Comments = booking.BkComments;
+            attendance.Booked = DateTime.Now;
+            attendance.Booker = userperson;
+            attendance.Modified = DateTime.Now;
+            attendance.Modifier = userperson;
+
             db.Attendances.Add(attendance);
+
+            //Update linked requirement
+            int StaffID = attendance.StaffID;
+            int CourseID = (int)sess.CourseID;
+            var req = db.Requirements.SingleOrDefault(r => r.StaffID == StaffID && r.CourseID == CourseID);
+            short OutID = booking.OutcomeID;
+            var outcome = db.Statuses.SingleOrDefault(s => s.ID == (OutID == 0 ? 2 : OutID));
+            if (req != null)
+            {
+                if (outcome.Requirement)
+                {
+                    req.Status = outcome;
+                }
+                else
+                {
+                    req.StatusID = (short)1;
+                }
+                req.Modifier = userperson;
+                req.Modified = DateTime.Now;
+            }
+
             await db.SaveChangesAsync();
 
-            return CreatedAtRoute("DefaultApi", new { id = attendance.ID }, attendance);
+            return CreatedAtRoute("DefaultApi", new { id = attendance.ID }, booking);
         }
 
         // DELETE: api/Attendances/5
---
+++ TDB_SAAS/DTOs/BookingDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDB_SAAS.DTOs
+{
+    public class BookingDTO
+    {
+        public int StaffID { get; set; }
+        public int SessID { get; set; }
+        public short OutcomeID { get; set; }
+        public String BkComments { get; set; }
+    }
+}

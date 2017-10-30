ea165c794cf5317515cb31d60527fe8cdd79e4bd
Steven Smith <stevensmith5@nhs.net>
2017-10-30 16:20:12 +0000

    Flag controllers and views added

---
+++ TDB_SAAS/Controllers/CFlagsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TDB_SAAS.Models;
+
+namespace TDB_SAAS.Controllers
+{
+    public class CFlagsController : ApiController
+    {
+        private TrainDB db = new TrainDB();
+
+        // GET: api/CFlags
+        public IQueryable<CFlag> GetCFlags()
+        {
+            return db.CFlags;
+        }
+
+        // GET: api/CFlags/5
+        [ResponseType(typeof(CFlag))]
+        public async Task<IHttpActionResult> GetCFlag(int id)
+        {
+            CFlag cFlag = await db.CFlags.FindAsync(id);
+            if (cFlag == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cFlag);
+        }
+
+        // PUT: api/CFlags/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCFlag(int id, CFlag cFlag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cFlag.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cFlag).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CFlagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CFlags
+        [ResponseType(typeof(CFlag))]
+        public async Task<IHttpActionResult> PostCFlag(CFlag cFlag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CFlags.Add(cFlag);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = cFlag.ID }, cFlag);
+        }
+
+        // DELETE: api/CFlags/5
+        [ResponseType(typeof(CFlag))]
+        public async Task<IHttpActionResult> DeleteCFlag(int id)
+        {
+            CFlag cFlag = await db.CFlags.FindAsync(id);
+            if (cFlag == null)
+            {
+                return NotFound();
+            }
+
+            db.CFlags.Remove(cFlag);
+            await db.SaveChangesAsync();
+
+            return Ok(cFlag);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CFlagExists(int id)
+        {
+            return db.CFlags.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ TDB_SAAS/Controllers/FlagsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TDB_SAAS.Models;
+
+namespace TDB_SAAS.Controllers
+{
+    public class FlagsController : Controller
+    {
+        private TrainDB db = new TrainDB();
+
+        // GET: Flags
+        public async Task<ActionResult> Index()
+        {
+            var flags = db.Flags.OrderBy(f => f.TheFlag);
+            ViewBag.CFlags = db.CFlags.OrderBy(f => f.TheFlag);
+
+            return View(await flags.ToListAsync());
+        }
+
+        // GET: Flags/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flag flag = await db.Flags.FindAsync(id);
+            if (flag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flag);
+        }
+
+        // GET: Flags/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: Flags/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,TheFlag")] Flag flag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Flags.Add(flag);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            
+            return View(flag);
+        }
+
+        // GET: Flags/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flag flag = await db.Flags.FindAsync(id);
+            if (flag == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(flag);
+        }
+
+        // POST: Flags/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,TheFlag,CreatorID,Created,ModifierID,Modified")] Flag flag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(flag).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(flag);
+        }
+
+        // GET: Flags/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Flag flag = await db.Flags.FindAsync(id);
+            if (flag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(flag);
+        }
+
+        // POST: Flags/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Flag flag = await db.Flags.FindAsync(id);
+            db.Flags.Remove(flag);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
